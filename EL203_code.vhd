65aae2bf1475e1c5c7b8b88bdd1a11a226477294
Jatin Ranpariya <jatinranpariya1510@gmail.com>
2022-11-29 17:28:27 +0530

    Done

--- EL203_code.vhd
+++ EL203_code.vhd
@@ -33,7 +33,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 -- any Xilinx primitives in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
-entity elevator is
+entity abcd is
 Port (  RESET: in STD_LOGIC;
         SBT: in STD_LOGIC;
         SMM: in STD_LOGIC;
@@ -51,13 +51,13 @@ Port (  RESET: in STD_LOGIC;
         state6: out STD_LOGIC;
         M0: out STD_LOGIC;
         M1: out STD_LOGIC;
-		led_f1: out STD_LOGIC;
-		led_f2: out STD_LOGIC;
-		led_f3: out STD_LOGIC
+		  led_f1: out STD_LOGIC;
+		  led_f2: out STD_LOGIC;
+		  led_f3: out STD_LOGIC
 );
-end elevator;
+end abcd;
 
-architecture Behavioral of elevator is 
+architecture Behavioral of abcd is 
 signal state:integer:=0;
 signal s3:std_logic:='0'; 
 signal s4:std_logic:='0';
@@ -66,150 +66,150 @@ signal s6:std_logic:='0';
 
 begin
     check:process (RESET, SBT, SMM, SMP, STP, PB1, PB2, PB3) 
-        begin
-        if (RESET='0') then
+    begin
+    if (RESET='0') then
+        state<=0;
+    end if;
+	 
+    if (PB1='1' and PB2='0' and PB3='0') then
+        if (state=1 or state=2) then
+            state<=6;
+        end if;
+    elsif (PB1='0' and PB2='1' and PB3='0') then 
+        if (state=0) then 
+            state<=3;
+        elsif (state=2) then
+            state<=5;
+        end if;
+    elsif (PB1='0' and PB2='0' and PB3='1') then 
+        if (state=0 or state=1) then
+            state<=4;
+        end if; 
+    end if;
+
+    if(SBT='1' and SMM='0' and SMP='0' and STP='0') then
+        if (state=6) then
             state<=0;
         end if;
-        
-        if (PB1='1' and PB2='0' and PB3='0') then
-            if (state=1 or state=2) then
-                state<=6;
-            end if;
-        elsif (PB1='0' and PB2='1' and PB3='0') then 
-            if (state=0) then 
-                state<=3;
-            elsif (state=2) then
-                state<=5;
-            end if;
-        elsif (PB1='0' and PB2='0' and PB3='1') then 
-            if (state=0 or state=1) then
-                state<=4;
-            end if; 
+    elsif (SBT='0' and SMM='1' and SMP='1' and STP='0') then
+        if (state=5 or state=3) then
+            state<=1;
         end if;
+    elsif (SBT='0' and SMM='0' and SMP='0' and STP='1') then
+        if (state=4) then
+            state<=2;
+        end if;
+    end if;
 
-        if(SBT='1' and SMM='0' and SMP='0' and STP='0') then
-            if (state=6) then
-                state<=0;
-            end if;
-        elsif (SBT='0' and SMM='1' and SMP='1' and STP='0') then
-            if (state=5 or state=3) then
-                state<=1;
-            end if;
-        elsif (SBT='0' and SMM='0' and SMP='0' and STP='1') then
-            if (state=4) then
-                state<=2;
-            end if;
-        end if;
+    if (state=0) then
+        state0 <= '1';
+        state1 <= '0'; 
+        state2 <= '0';
+        state3 <= '0';
+        s3 <= '0';
+        s4 <= '0';
+        state4 <= '0';
+        state5 <= '0';
+        s5 <= '0';
+        s6 <= '0';
+        state6 <= '0';
+		  led_f1 <= '1';
+		  led_f2 <= '0';
+		  led_f3 <= '0';
+    elsif (state=1) then
+        state0 <= '0';
+        state1 <= '1'; 
+        state2 <= '0';
+        state3 <= '0';
+        s3 <= '0';
+        s4 <= '0';
+        state4 <= '0';
+        state5 <= '0';
+        s5 <= '0';
+        s6 <= '0';
+        state6 <= '0';
+		  led_f1 <= '0';
+		  led_f2 <= '1';
+		  led_f3 <= '0';
+    elsif (state=2) then
+        state0 <= '0';
+        state1 <= '0'; 
+        state2 <= '1';
+        state3 <= '0';
+        s3 <= '0';
+        s4 <= '0';
+        state4 <= '0';
+        state5 <= '0';
+        s5 <= '0';
+        s6 <= '0';
+        state6 <= '0';
+		  led_f1 <= '0';
+		  led_f2 <= '0';
+		  led_f3 <= '1';
+    elsif (state=3) then
+        state0 <= '0';
+        state1 <= '0'; 
+        state2 <= '0';
+        state3 <= '1';
+        s3 <= '1';
+        s4 <= '0';
+        state4 <= '0';
+        state5 <= '0';
+        s5 <= '0';
+        s6 <= '0';
+        state6 <= '0';
+		  led_f1 <= '0';
+		  led_f2 <= '0';
+		  led_f3 <= '0';
+    elsif (state=4) then
+        state0 <= '0';
+        state1 <= '0'; 
+        state2 <= '0';
+        state3 <= '0';
+        s3 <= '0';
+        s4 <= '1';
+        state4 <= '1';
+        state5 <= '0';
+        s5 <= '0';
+        s6 <= '0';
+        state6 <= '0';
+		  led_f1 <= '0';
+		  led_f2 <= '0';
+		  led_f3 <= '0';
+    elsif (state=5) then
+        state0 <= '0';
+        state1 <= '0'; 
+        state2 <= '0';
+        state3 <= '0';
+        s3 <= '0';
+        s4 <= '0';
+        state4 <= '0';
+        state5 <= '1';
+        s5 <= '1';
+        s6 <= '0';
+        state6 <= '0';
+		  led_f1 <= '0';
+		  led_f2 <= '0';
+		  led_f3 <= '0';
+    elsif (state=6) then
+        state0 <= '0';
+        state1 <= '0'; 
+        state2 <= '0';
+        state3 <= '0';
+        s3 <= '0';
+        s4 <= '0';
+        state4 <= '0';
+        state5 <= '0';
+        s5 <= '0';
+        s6 <= '1';
+        state6 <= '1';
+		  led_f1 <= '0';
+		  led_f2 <= '0';
+		  led_f3 <= '0';
+    end if;
 
-        if (state=0) then
-            state0 <= '1';
-            state1 <= '0'; 
-            state2 <= '0';
-            state3 <= '0';
-            state4 <= '0';
-            state5 <= '0';
-            state6 <= '0';
-            s3 <= '0';
-            s4 <= '0';
-            s5 <= '0';
-            s6 <= '0';
-            led_f1 <= '1';
-            led_f2 <= '0';
-            led_f3 <= '0';
-        elsif (state=1) then
-            state0 <= '0';
-            state1 <= '1'; 
-            state2 <= '0';
-            state3 <= '0';
-            state4 <= '0';
-            state5 <= '0';
-            state6 <= '0';
-            s3 <= '0';
-            s4 <= '0';
-            s5 <= '0';
-            s6 <= '0';
-            led_f1 <= '0';
-            led_f2 <= '1';
-            led_f3 <= '0';
-        elsif (state=2) then
-            state0 <= '0';
-            state1 <= '0'; 
-            state2 <= '1';
-            state3 <= '0';
-            state4 <= '0';
-            state5 <= '0';
-            state6 <= '0';
-            s3 <= '0';
-            s4 <= '0';
-            s5 <= '0';
-            s6 <= '0';
-            led_f1 <= '0';
-            led_f2 <= '0';
-            led_f3 <= '1';
-        elsif (state=3) then
-            state0 <= '0';
-            state1 <= '0'; 
-            state2 <= '0';
-            state3 <= '1';
-            state4 <= '0';
-            state5 <= '0';
-            state6 <= '0';
-            s3 <= '1';
-            s4 <= '0';
-            s5 <= '0';
-            s6 <= '0';
-            led_f1 <= '0';
-            led_f2 <= '0';
-            led_f3 <= '0';
-        elsif (state=4) then
-            state0 <= '0';
-            state1 <= '0'; 
-            state2 <= '0';
-            state3 <= '0';
-            state4 <= '1';
-            state5 <= '0';
-            state6 <= '0';
-            s3 <= '0';
-            s4 <= '1';
-            s5 <= '0';
-            s6 <= '0';
-            led_f1 <= '0';
-            led_f2 <= '0';
-            led_f3 <= '0';
-        elsif (state=5) then
-            state0 <= '0';
-            state1 <= '0'; 
-            state2 <= '0';
-            state3 <= '0';
-            state4 <= '0';
-            state5 <= '1';
-            state6 <= '0';
-            s3 <= '0';
-            s4 <= '0';
-            s5 <= '1';
-            s6 <= '0';
-            led_f1 <= '0';
-            led_f2 <= '0';
-            led_f3 <= '0';
-        elsif (state=6) then
-            state0 <= '0';
-            state1 <= '0'; 
-            state2 <= '0';
-            state3 <= '0';
-            state4 <= '0';
-            state5 <= '0';
-            state6 <= '1';
-            s3 <= '0';
-            s4 <= '0';
-            s5 <= '0';
-            s6 <= '1';
-            led_f1 <= '0';
-            led_f2 <= '0';
-            led_f3 <= '0';
-        end if;
     end process;
 
     M0 <= s5 or s6;
     M1 <= s3 or s4;
-    
 end Behavioral;
